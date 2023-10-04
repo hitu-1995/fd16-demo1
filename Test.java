d045502125bf825fef583ecb6566f29c35b3c29a
hitu-1995 <hiteshwankhede11@gmail.com>
2023-10-04 13:01:56 +0530

    Second commit

--- Test.java
+++ Test.java
@@ -3,7 +3,8 @@ public class Test {
   public static void main(String []args) {
 
     System.out.println("dev1 added main method");
-    System.out.println("dev2 added line 1");
+    System.out.println("dev1 added line 1");
+    System.out.println("dev1 added line 2");
 
   }
 
